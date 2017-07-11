77949243756782ba6a30e01105e83a4a726c6dcc
Nhan Tran <trdainhan@gmail.com>
2017-07-11 09:32:39 +0700

    Improve code

--- utils/CookieUtils.js
+++ utils/CookieUtils.js
@@ -1,7 +1,7 @@
 export function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires="+d.toUTCString();
+  var expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + "HttpOnly;" + ";path=/";
 }
 
@@ -10,10 +10,10 @@ export function getCookie(cname) {
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
